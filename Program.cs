2502f46c04319e55b173731c63128e4c4622d586
drmodig <n.e.h.carlsson@gmail.com>
2020-07-21 18:46:24 +0200

    simple fizzbuzz

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace fizzbuzz
+{
+    class MainClass
+    {
+        public static void Main(string[] args)  //Simple fizzbuzz solution
+        {
+            int n = 1;
+            while (n <= 100)
+            {
+                if (n % 3 == 0 && n % 5 == 0)
+                {
+                    Console.WriteLine("Fizzbuzz");
+                }
+                else if (n % 3 == 0)
+                {
+                    Console.WriteLine("Fizz");
+                }
+                else if (n % 5 == 0)
+                {
+                    Console.WriteLine("Buzz");
+                }
+                else
+                {
+                    Console.WriteLine(n);
+                }
+                n++;
+            }
+        }
+    }
+}
